9535342af54d21e424de6e0aecaba0334c43481c
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-10-25 11:58:25 +0300

    re add my project

--- fav.js
+++ fav.js
@@ -1,4 +1,15 @@
-const a = JSON.parse(localStorage.getItem("listGame"))
+const arr = JSON.parse(localStorage.getItem("listGame"));
+let arrFav = arr.filter((listGame) => listGame.fav);
 
-console.log(a);
-
+const rinderFav = () => {
+  for (let i = 0; i < arrFav.length; i++) {
+    $(".ulGamesFav").append(
+      `<div class='favPage'>
+       <div class='favItem'> <div class='favImg'><img src="${arrFav[i].imgUrl}" alt="Game Logo"> </div>
+          <div class='favH2'> <p>${arrFav[i].Name}</p> </div>
+           </div> 
+           </div>`
+    );
+  }
+};
+rinderFav();
--- firstProject.js
+++ firstProject.js
@@ -1,11 +1,11 @@
 // array of object with img and name of the games
-
-const listGame = [
+let listGame = JSON.parse(localStorage.getItem("listGame"));
+const listGameStorage = [
   {
     id: 1,
     Name: "DESTINY 2",
     imgUrl: "/img/Destiny2.jpg",
-    gamePage: "destinyPage.html",
+
     desc: "MultiPlayer game 2-12 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -15,7 +15,7 @@ const listGame = [
     id: 2,
     Name: "CALL OF DUTY MW",
     imgUrl: "/img/callofduty.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2-152 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -24,7 +24,7 @@ const listGame = [
     id: 3,
     Name: "BATTLEFIELD 2042",
     imgUrl: "/img/bf.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2-128 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -33,7 +33,7 @@ const listGame = [
     id: 4,
     Name: "DIABLO 3",
     imgUrl: "/img/diablo3.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2-4 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -42,7 +42,7 @@ const listGame = [
     id: 5,
     Name: "GHOST OF TSUSHIMA",
     imgUrl: "/img/ghost.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2-4 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -51,7 +51,7 @@ const listGame = [
     id: 6,
     Name: "SEKIRO",
     imgUrl: "/img/sekiro.jpg",
-    gamePage: "#",
+
     desc: "single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -60,7 +60,7 @@ const listGame = [
     id: 7,
     Name: "SHADOW OF WAR",
     imgUrl: "/img/sow.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2 Players And single player",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
@@ -69,39 +69,37 @@ const listGame = [
     id: 8,
     Name: "GTA V",
     imgUrl: "/img/gtav.jpg",
-    gamePage: "#",
+
     desc: "MultiPlayer game 2-64 Players And single player ",
     info: "is a free-to-play online-only multiplayer first-person shooter video game developed by Bungie. It was originally released as a pay to play game in 2017 for the PlayStation 4, Xbox One, and Microsoft Windows platforms",
     fav: false,
   },
 ];
 
-// second page 
-const secP = (i) => {
-  $(".ul1").hide();
+// Value of listGame
+if (listGame == null) {
+  listGame = [...listGameStorage];
+  localStorage.setItem("listGame", JSON.stringify(listGame));
+}
 
-  $(".descP").append(
-    `<div class="descImg"><img src="${listGame[i].imgUrl}" alt="Game Logo"/> </div>  <div class='newPage'> <h2>${listGame[i].Name}</h2><p>${listGame[i].info}</div>`
-  );
-  rinder();
-};
 
-// fav page 
+// fav page
 const addToFav = (i) => {
-// if (listGame[i].fav == false){
-//   listGame[i].fav = true
-// }else {
-//   listGame[i].fav = false
-// }
-console.log(listGame[i]);
+  console.log(listGame[i].fav);
+  if (listGame[i].fav == false) {
+    listGame[i].fav = true;
+  } else {
+    listGame[i].fav = false;
+  }
+  console.log(listGame[i].fav);
 
-localStorage.setItem('listGame',JSON.stringify(listGame[i]))
-
+  localStorage.setItem("listGame", JSON.stringify(listGame));
 };
-
+let max = 4;
+let i = 0;
 // rinder
 const rinder = () => {
-  for (let i = 0; i < listGame.length; i++) {
+  for (i; i < max; i++) {
     $(".ulGames").append(
       `<div class='li1'>
        <li>
@@ -109,7 +107,6 @@ const rinder = () => {
         <p onclick=secP(${i}) style= cursor:pointer>${listGame[i].Name}</p>
         <p>${listGame[i].desc}</p>
         <button class='btnFav btn btn-danger' onclick=addToFav(${i})>Add To Favorite </button>
-
         </button> 
         </li>
         </div>`
@@ -117,3 +114,29 @@ const rinder = () => {
   }
 };
 rinder();
+
+
+// second page
+const secP = (i) => {
+  $(".ul1").hide();
+  $(".btnCenter").hide()
+
+  $(".descP").append(
+    `<div class="descImg"> <img src="${listGame[i].imgUrl}" alt="Game Logo"/> </div>
+      <div class='newPage'> <h2>${listGame[i].Name}</h2><p>${listGame[i].info}</div>
+     <div>  <button class='btnFav btn btn-danger'id='btnFavSecPage' onclick=addToFav(${i})>Add To Favorite </button>  </div>`
+  );
+  rinder();
+};
+
+
+
+
+
+
+$(".showMore").on("click", () => {
+  i = 4;
+  max = listGame.length;
+  rinder();
+  $(".showMore").remove();
+});
